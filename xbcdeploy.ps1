8c68bad582364100868c4ee87c6ad1b809b3ab1a
zcmyron <zcmyron@users.noreply.github.com>
2021-03-22 20:45:00 +0800

    Create xbcdeploy.ps1

---
+++ xbcdeploy.ps1
@@ -0,0 +1,8 @@
+$src = 'https://api-us1.xbc.trendmicro.com/api/public/download_xbc_agent/v1.0/7329d0a3-f04c-447e-b4c4-67a99ec43176'
+$des = "c:\xbc.exe"
+Invoke-WebRequest -uri $src -OutFile $des
+Unblock-File $des
+
+sleep 100
+
+start c:\xbc.exe
