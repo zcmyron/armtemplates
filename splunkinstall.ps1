cefccccb69b2efd606ee33fce087c7fbbf070545
zcmyron <zcmyron@users.noreply.github.com>
2021-03-23 17:07:27 +0800

    Update splunkinstall.ps1

--- splunkinstall.ps1
+++ splunkinstall.ps1
@@ -4,3 +4,4 @@ Invoke-WebRequest -uri $src -OutFile $des
 sleep 300
 Unblock-File $des
 msiexec.exe /i c:\splunk-8.1.3-63079c59e632-x64-release.msi AGREETOLICENSE=Yes SPLUNKUSERNAME=SplunkAdmin SPLUNKPASSWORD=MyNewPassword /q /l*v C:\SplunkInstall.log
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/zcmyron/armtemplates/main/dashboard_template.xml" -OutFile "c:\dashboard_template.xml"
